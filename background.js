848f1e96b14ae1739325af5eb4a2c236d28ee0c2
shmutalov <shmutalov@gmail.com>
2021-08-02 13:43:40 +0500

    Updated to more user-friendly download logic

--- background.js
+++ background.js
@@ -1,18 +1,22 @@
 // New extension background page, fetching from a known URL and relaying data:
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
 	if (request.contentScriptQuery == "checkUrl") {
-		fetch(request.url, {
-			"method": "HEAD"
-		})
-		.then(r => {
-			console.log(`${r.status} ${r.statusText}`)
-			sendResponse(r.ok)
-		})
-		.catch(error => {
-			console.log(error)
-			sendResponse(false)
-		})
-		
-		return true;  // Will respond asynchronously.
-    }
+		fetch(request.url, { "method": "HEAD" })
+			.then(r => sendResponse(r.ok))
+			.catch(_ => sendResponse(false))
+	}
+
+	if (request.contentScriptQuery == "downloadUrl") {
+		chrome.downloads.download({ url: request.url })
+	}
+
+	return true  // Will respond asynchronously.
+})
+
+chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
+	if (request.contentScriptQuery == "downloadUrl") {
+		chrome.downloads.download({ url: request.url })
+	}
+
+	return true
 })
--- src/inject.js
+++ src/inject.js
@@ -2,36 +2,19 @@ const VIDEO_URL = '//v.mover.uz/';
 var fullVideoUrl = "";
 
 var qualityMap = {
-	"240p": "_s", 
-	"360p": "_m", 
-	"480p": "_b", 
+	"240p": "_s",
+	"360p": "_m",
+	"480p": "_b",
 	"720p": "_h"
 };
 
-var currentTab = false;
-var isOverHttps = false;
-
 function getVideoId(url) {
 	console.log(`Extracting the video id from ${url}...`)
-	var needleLen = 0
-	
-	if (url.lastIndexOf("https", 0) === 0) {
-		isOverHttps = true
-	}
-	
-	var needleLen = isOverHttps 
-		? "https://mover.uz/watch/".length
-		: "http://mover.uz/watch/".length
-	
-	fullVideoUrl = isOverHttps
-		? "https:" + VIDEO_URL
-		: "http:" + VIDEO_URL
-	
-	var videoId = url.substr(needleLen)
+	const u = new URL(url)
+	const segments = u.pathname.split('/')
+	const videoId = segments.pop() || segments.pop()
 
-	if (videoId.lastIndexOf("/") > 0) {
-		videoId = videoId.substr(0, videoId.length-1)
-	}
+	fullVideoUrl = u.protocol + VIDEO_URL
 
 	console.log(`Video id = ${videoId}`)
 	return videoId
@@ -39,7 +22,7 @@ function getVideoId(url) {
 
 async function pingURI(uri) {
 	return await new Promise(resolve => {
-		chrome.runtime.sendMessage({contentScriptQuery: "checkUrl", url: uri}, result => {
+		chrome.runtime.sendMessage({ contentScriptQuery: "checkUrl", url: uri }, result => {
 			resolve(result)
 		})
 	});
@@ -48,15 +31,7 @@ async function pingURI(uri) {
 async function checkVideo(url, qualitySupportMap, quality) {
 	console.log(`Checking quality [${quality}] => ${url}`)
 
-	let exist = await pingURI(url)
-	
-	if (exist) {
-		console.log(`Quality [${quality}] is supported`)
-	} else {
-		console.log(`Quality [${quality}] isn't supported`)
-	}
-
-	qualitySupportMap[quality] = exist
+	qualitySupportMap[quality] = await pingURI(url)
 }
 
 async function getVideoQualityPrefixes(videoId) {
@@ -67,28 +42,30 @@ async function getVideoQualityPrefixes(videoId) {
 	await Promise.all(Object.entries(qualityMap).map(async ([quality, postfix]) => {
 		await checkVideo(`${fullVideoUrl}${videoId}${postfix}.mp4`, qualitySupportMap, quality)
 	}))
-	
+
 	return qualitySupportMap
 }
 
 async function addDownloadButton() {
 	let currentPageURI = window.location.href
-	
+
 	// check current page
 	if (currentPageURI.indexOf("mover.uz/watch/") == -1)
 		return
-	
+
 	let videoId = getVideoId(currentPageURI)
-	
+
 	// check video quality types availability
 	let qualitySupportMap = await getVideoQualityPrefixes(videoId)
 	// build elements html
 	var qualityListHTML = '<span>Скачать: '
-	
+
 	var filtered = Object.entries(qualitySupportMap)
 		.filter(([_, v]) => v)
 		.map(x => x[0])
-	var filteredArrayLength = filtered.length
+		.sort()
+
+	const filteredArrayLength = filtered.length
 
 	if (!filteredArrayLength) {
 		console.log(`No supported video qualities`)
@@ -97,16 +74,16 @@ async function addDownloadButton() {
 
 	console.log(`Quality support: ${filtered}`)
 
-	let btnClass = "btn btn-medium btn-blue"
+	const btnClass = "btn btn-medium btn-blue"
+	const extensionId = chrome.runtime.id
 
 	filtered.forEach(quality => {
-		var prefix = qualityMap[quality]
-		
-		var videoUrl = fullVideoUrl + videoId + prefix + ".mp4"
-		
-		qualityListHTML += `<a class="${btnClass}" href="${videoUrl}" download>${quality}</a>`
+		const prefix = qualityMap[quality]
+		const videoUrl = fullVideoUrl + videoId + prefix + ".mp4"
+
+		qualityListHTML += `<a class="${btnClass}" href="#" onclick='chrome.runtime.sendMessage("${extensionId}", {contentScriptQuery: "downloadUrl", url: "${videoUrl}"}); event.preventDefault ? event.preventDefault() : event.returnValue = false;'>${quality}</a>`
 	});
-	
+
 	qualityListHTML += '</span><div class="clmar3"></div>'
 
 	console.log("Searching the element to add our buttons after...")
@@ -116,7 +93,7 @@ async function addDownloadButton() {
 
 	if (!statistics)
 		return
-	
+
 	statistics.insertAdjacentHTML('afterend', qualityListHTML)
 	console.log(`Download videos buttons added`)
 }
